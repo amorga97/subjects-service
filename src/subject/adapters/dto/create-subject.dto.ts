b75298355b3afdb4b7c2f49eb56ed747aa878ead
Antonio Morga <anotniomc9719@gmail.com>
2022-11-09 18:38:59 +0100

    Added validation to createSubjectDto

--- src/subject/adapters/dto/create-subject.dto.ts
+++ src/subject/adapters/dto/create-subject.dto.ts
@@ -1,7 +1,16 @@
+import { IsString, MaxLength, ValidateNested } from 'class-validator';
+
 export class CreateSubjectDto {
+  @IsString()
+  @MaxLength(200)
   title: string;
+  @IsString()
   author: string;
+  @IsString()
   description?: string;
+  @IsString()
   institution?: string;
+  @IsString()
+  @ValidateNested({ each: true })
   meta_data?: [string, string][];
 }
