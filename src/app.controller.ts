6c691b21240e534ad835ad226949552bd2fbe348
Antonio Morga <anotniomc9719@gmail.com>
2022-11-12 13:20:44 +0100

    Removed unnecessary code from app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,13 @@
-import { Controller, Get, Inject, Logger } from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   logger = new Logger('Users');
-  constructor(
-    private readonly appService: AppService,
-    @Inject('COURSES') private readonly coursesClient: ClientKafka,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    this.coursesClient.emit('testing', {
-      testData: 'testing',
-      moreData: ['test', 'test'],
-    });
     return this.appService.getHello();
   }
 }
